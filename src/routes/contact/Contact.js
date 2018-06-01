4f1c361d3795e0af97e2eeef4464d76dc8d982d9
Flavian Buteau <flavian.buteau@affinitas.de>
2018-06-01 18:39:30 +0200

    some dev for contact form

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -5,7 +5,20 @@ class Contact extends Component {
   render() {
     return (
       <div className={styles.Contact}>
-        contact page to do
+        <div>contact page to do</div>
+        <form
+            className={styles.ContactForm}
+            name='afm_form_af054f1d'
+            id='afm_form_af054f1d'
+            action='http://free.allforms.mailjol.net/u/af054f1d.php'
+            method='POST'
+          >
+          <input type='text' name='name' placeholder='name*' />
+          <input type='text' name='email' placeholder='email*' />
+          <input type='text' name='subject' placeholder='subject*' />
+          <textarea rows='4' name='message' placeholder='message'></textarea>
+          <input type='submit' name='submit' value='send' />
+        </form>
       </div>
     )
   }
