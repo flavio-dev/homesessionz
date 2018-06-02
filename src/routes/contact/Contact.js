6b5da10c22910befbad633567a4272389f46ef82
flavio-dev <trash_flavio@hotmail.fr>
2018-06-02 19:10:43 +0200

    videos

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -7,16 +7,16 @@ class Contact extends Component {
       <div className={styles.Contact}>
         <div>contact page to do</div>
         <form
-            className={styles.ContactForm}
-            name='afm_form_af054f1d'
-            id='afm_form_af054f1d'
-            action='http://free.allforms.mailjol.net/u/af054f1d.php'
-            method='POST'
-          >
+          className={styles.ContactForm}
+          name='afm_form_af054f1d'
+          id='afm_form_af054f1d'
+          action='http://free.allforms.mailjol.net/u/af054f1d.php'
+          method='POST'
+        >
           <input type='text' name='name' placeholder='name*' />
           <input type='text' name='email' placeholder='email*' />
           <input type='text' name='subject' placeholder='subject*' />
-          <textarea rows='4' name='message' placeholder='message'></textarea>
+          <textarea rows='4' name='message' placeholder='message' />
           <input type='submit' name='submit' value='send' />
         </form>
       </div>
--- src/routes/home/Home/Home.js
+++ src/routes/home/Home/Home.js
@@ -5,6 +5,10 @@ import CloudcastHomePlacebo from 'components/CloudcastHomeContainer/CloudcastHom
 
 import styles from './Home.css'
 
+import smallHomeVid from './homeSmall.m4v'
+import mediumHomeVid from './homeMedium.m4v'
+import largeHomeVid from './homeLarge.m4v'
+
 class Home extends Component {
   constructor(props) {
     super(props)
@@ -26,19 +30,34 @@ class Home extends Component {
 
   render() {
     return (
-      <div className={styles.Home}>
-        {this.state.listCloudcastKeys.map((key, index) => {
-          const cloudcast = this.state.cloudcastDetails[key]
-          if (cloudcast) {
-            return <CloudcastHomeContainer cloudcast={cloudcast} key={key} />
-          } else {
-            return null
-          }
-        })}
-        <CloudcastHomePlacebo />
-        <CloudcastHomePlacebo />
-        <CloudcastHomePlacebo />
-        <CloudcastHomePlacebo />
+      <div>
+        <div className={styles.HomeVideoWrapper}>
+          <video autoPlay muted loop className={styles.HomeVideoSmall}>
+            <source src={smallHomeVid} type='video/mp4' />
+          </video>
+          <video autoPlay muted loop className={styles.HomeVideoMedium}>
+            <source src={mediumHomeVid} type='video/mp4' />
+          </video>
+          <video autoPlay muted loop className={styles.HomeVideoLarge}>
+            <source src={largeHomeVid} type='video/mp4' />
+          </video>
+        </div>
+        <div className={styles.HomeGridWrapper}>
+          <div className={styles.HomeGrid}>
+            {this.state.listCloudcastKeys.map((key, index) => {
+              const cloudcast = this.state.cloudcastDetails[key]
+              if (cloudcast) {
+                return <CloudcastHomeContainer cloudcast={cloudcast} key={key} />
+              } else {
+                return null
+              }
+            })}
+            <CloudcastHomePlacebo />
+            <CloudcastHomePlacebo />
+            <CloudcastHomePlacebo />
+            <CloudcastHomePlacebo />
+          </div>
+        </div>
       </div>
     )
   }
