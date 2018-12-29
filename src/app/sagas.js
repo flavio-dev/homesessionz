5c8aa4f793138e1abce26a6943bdfe4309deb5d9
flavio-dev <trash_flavio@hotmail.fr>
2018-12-29 16:59:21 +0100

    removing temp files for offline

--- src/app/sagas.js
+++ src/app/sagas.js
@@ -3,17 +3,6 @@ import { fork, put, call } from 'redux-saga/effects'
 import whatwgFetch from 'utils/fetch'
 import getEnvUrlPrefix from 'utils/envUrl'
 
-// TEST data
-// import listMixes from 'temp/data.json'
-// import mixDetails0 from 'temp/offlinedata0.json'
-// import mixDetails1 from 'temp/offlinedata1.json'
-// import mixDetails2 from 'temp/offlinedata2.json'
-// import mixDetails3 from 'temp/offlinedata3.json'
-// import mixDetails4 from 'temp/offlinedata4.json'
-// import mixDetails5 from 'temp/offlinedata5.json'
-// import mixDetails6 from 'temp/offlinedata6.json'
-// END TEST data
-
 import {
   setCloudcastDetails,
   setIndexIsFeaturedCloudcast,
@@ -25,7 +14,6 @@ export function* getInitialListMixesFromGithub() {
     whatwgFetch,
     'https://raw.githubusercontent.com/flavio-dev/homesessionzserver/master/data.json'
   )
-  // const initialListOfMixes = listMixes
   var dateObj = new Date()
   var day = dateObj.getDate()
   const indexFeatured = (((initialListOfMixes.length + 3) * day) % (initialListOfMixes.length - 1)) + 1
@@ -44,32 +32,6 @@ function* getCloudcastDetailsCall(cloudcastKey, index, isFeatured) {
   const url = prefixUrl + '/cloudcast' + cloudcastKey
   try {
     const cloudDetails = yield call(whatwgFetch, url)
-    // let cloudDetails
-    // switch (index) {
-    //   case 0:
-    //     cloudDetails = mixDetails0
-    //     break
-    //   case 1:
-    //     cloudDetails = mixDetails1
-    //     break
-    //   case 2:
-    //     cloudDetails = mixDetails2
-    //     break
-    //   case 3:
-    //     cloudDetails = mixDetails3
-    //     break
-    //   case 4:
-    //     cloudDetails = mixDetails4
-    //     break
-    //   case 5:
-    //     cloudDetails = mixDetails5
-    //     break
-    //   case 6:
-    //     cloudDetails = mixDetails6
-    //     break
-    //   default:
-    //     cloudDetails = mixDetails0
-    // }
 
     yield put(setCloudcastDetails(cloudDetails, index))
     if (isFeatured) {
