7aa6cbe6511525a5cb28f1659ba1124fbb1090a0
Flavian Buteau <flavian.buteau@affinitas.de>
2019-02-04 12:15:37 +0100

    does that suspense work?

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import PropTypes from 'prop-types'
 import { ConnectedRouter } from 'connected-react-router'
 import { Route, Switch } from 'react-router-dom'
@@ -6,15 +6,16 @@ import { Provider } from 'react-redux'
 import { ToastContainer, Slide } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.min.css'
 
-import HomeContainer from 'routes/home/HomeContainer'
-import CloudcastDetailsContainer from 'routes/cloudcastDetails/CloudcastDetailsContainer'
-import Contact from 'routes/contact/Contact'
-import About from 'routes/about/About'
 import LayoutContainer from 'components/LayoutContainer'
 import CloseIcon from 'components/CloseIcon'
 import scrollIntoView from 'hocomponents/ScrollIntoView'
 
 import 'index.css'
+
+const HomeContainer = lazy(() => import('routes/home/HomeContainer'))
+const CloudcastDetailsContainer = lazy(() => import('routes/cloudcastDetails/CloudcastDetailsContainer'))
+const Contact = lazy(() => import('routes/contact/Contact'))
+const About = lazy(() => import('routes/about/About'))
 
 export const Root = ({ store, history }) => (
   <Provider store={store}>
@@ -26,12 +27,14 @@ export const Root = ({ store, history }) => (
     />
     <ConnectedRouter history={history}>
       <LayoutContainer>
-        <Switch>
-          <Route exact path='/' component={HomeContainer} />
-          <Route exact path='/contact' component={scrollIntoView(Contact)} />
-          <Route exact path='/about' component={scrollIntoView(About)} />
-          <Route exact path='/:cloudcastId' component={scrollIntoView(CloudcastDetailsContainer)} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path='/' component={HomeContainer} />
+            <Route exact path='/contact' component={scrollIntoView(Contact)} />
+            <Route exact path='/about' component={scrollIntoView(About)} />
+            <Route exact path='/:cloudcastId' component={scrollIntoView(CloudcastDetailsContainer)} />
+          </Switch>
+        </Suspense>
       </LayoutContainer>
     </ConnectedRouter>
   </Provider>
