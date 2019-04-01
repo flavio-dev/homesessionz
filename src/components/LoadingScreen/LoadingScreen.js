cc2dd673682f845c868eaa4da683e5f59fa54d7a
flavio-dev <trash_flavio@hotmail.fr>
2019-04-01 21:33:03 +0200

    line height

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -6,7 +6,8 @@ import './LoadingScreen.css'
 export const LoadingScreen = () => (
   <div className='ls'>
     <div className='ls__content'>
-      <p className='ls__icon'><VinylIcon /></p>
+      <div className='ls__icon'><VinylIcon /></div>
+      <p />
       <p className='ls__text font--medium'>loading...</p>
     </div>
   </div>
