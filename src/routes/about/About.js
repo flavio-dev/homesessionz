d63992e58989f8d4748522d9a3b3a6d993b9da61
flavio-dev <trash_flavio@hotmail.fr>
2019-09-18 18:21:16 +0200

    new about

--- src/routes/about/About.js
+++ src/routes/about/About.js
@@ -20,15 +20,18 @@ export const About = () => (
         <div className='about'>
           <h2 className='title-margin'>høme sessiønz...</h2>
           <p>
-            ... are a series of events designed to make a connection between a music selector & a small audience.
+            ... are a series of events designed to make a connection between a music selector,
+            a visual artist & a small audience.
             <br />
             no booth, no phones, no physical barriers.<br />
-            h/s aims to rebuild the bond between interaction & communication, music & people.
+            h/s aims to rebuild the bond between interaction & communication, music & people, art & accessibility.
           </p>
           <p>
-            no live streams, no requests. just freedom.<br />
-            freedom for the selector to play whatever they feel like, for however long they want - no pressure.<br />
-            let the music tell the story.
+            no live streams, no requests, no pressure. just freedom & mind openers.<br />
+            freedom for the selector to play whatever they feel like, for however long they want.<br />
+            opportunity for the artist to share his/her art expression directly with the audience.<br />
+            this results in a unique journey, between impromptu cross art form collaborations & free flow,
+            in the intimacy of a private location.
           </p>
         </div>
       </section>
