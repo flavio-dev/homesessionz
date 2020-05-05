ac16cabb774f738d978afecec63b4e1c86f72fe6
flavio-dev <trash_flavio@hotmail.fr>
2020-05-05 13:52:54 +0200

    Update Tag.js

--- src/components/TagContainer/Tag/Tag.js
+++ src/components/TagContainer/Tag/Tag.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import './Tag.css'
 
 export const Tag = ({ name, style, placeholder, setSearchText }) => {
-  const newName = name.replace(/^#/, '');
+  const newName = name.replace(/^#/, '')
   return <span
     className={placeholder ? 'font--small tag tag--placeholder button-style' : 'font--small tag button-style'}
     style={style}
